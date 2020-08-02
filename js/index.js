92d072d45ce3565b31ace3ad06f699ec0858dba0
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2020-08-02 01:38:50 -0400

    Finalizing the website :)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
-function closeAlert() {
-	document.getElementById("warning").style.opacity = "0";
-	setTimeout(() => {
-		document.getElementById("warning").style.display = "none";
-	}, 500);
-}
+document.getElementById('name').style.transform = 'translate(-120px, -120px)';
+
+var tl = anime.timeline({
+	easing: 'easeInOutExpo',
+	duration: 1250,
+});
+
+tl.add({
+	targets: '#name',
+	translateX: 120,
+	translateY: 120,
+	opacity: 1,
+})
+	.add(
+		{
+			targets: '#name-underline',
+			width: 250,
+		},
+		'-=500',
+	)
+	.add(
+		{
+			targets: '#section-1-footer',
+			opacity: 1,
+		},
+		'-=500',
+	);
